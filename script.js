7c2d14a8bec600fdfba7e3c5183578d7d6d4ad73
MaksJohnson <kashinmaksim03@mail.ru>
2021-02-23 19:30:46 +0500

    картинки
    Сжал картинки, изменил код под них
    теперь центральная часть обрезается под размер экрана

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ $(window).scroll(function() {
     $('nav').removeClass('bg-transparent');
   }
 });
+
+jQuery('ul.nav > li').hover(function() {
+  jQuery(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn();
+}, function() {
+  jQuery(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut();
+})

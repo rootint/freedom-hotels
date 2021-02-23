2bad4bb473fe7b9130f645684a078b20356deaf3
MaksJohnson <kashinmaksim03@mail.ru>
2021-02-23 22:43:35 +0500

    почти готово

--- script.js
+++ script.js
@@ -5,19 +5,47 @@ $(window).scroll(function() {
     $('nav').removeClass('bg-transparent');
   }
 });
+// $(document).ready(function () {
+//     $('.dropdown-toggle').mouseover(function() {
+//         $('.dropdown-menu').show();
+//     })
+//     $('.dropdown-toggle').mouseout(function() {
+//         t = setTimeout(function() {
+//             $('.dropdown-menu').hide();
+//         }, 100);
+//         $('.dropdown-menu').on('mouseenter', function() {
+//             $('.dropdown-menu').show();
+//             clearTimeout(t);
+//         }).on('mouseleave', function() {
+//             $('.dropdown-menu').hide();
+//         })
+//     })
+// })
 $(document).ready(function () {
-    $('.dropdown-toggle').mouseover(function() {
-        $('.dropdown-menu').show();
+	$('.dropdown-toggle').off('hidden');
+	$(document).off('dropdown')
+	$('.dropdown-toggle').unbind();
+	$('.dropdown-toggle').on("click", function(e) {
+    	document.location.href = "file:///C:/Users/maksi/OneDrive/Документы/GitHub/freedom-hotels/room_list.html";
     })
-    $('.dropdown-toggle').mouseout(function() {
-        t = setTimeout(function() {
-            $('.dropdown-menu').hide();
-        }, 100);
-        $('.dropdown-menu').on('mouseenter', function() {
-            $('.dropdown-menu').show();
-            clearTimeout(t);
-        }).on('mouseleave', function() {
-            $('.dropdown-menu').hide();
-        })
-    })
+    // $('.dropdown-toggle').mouseover(function() {
+    //     $('.dropdown-menu').show();
+    // })
+    // $('.dropdown-toggle').mouseout(function() {
+    //     t = setTimeout(function() {
+    //         $('.dropdown-menu').hide();
+    //     }, 100);
+    //     $('.dropdown-menu').on('mouseenter', function() {
+    //         $('.dropdown-menu').show();
+    //         clearTimeout(t);
+    //     }).on('mouseleave', function() {
+    //         $('.dropdown-menu').hide();
+    //     })
+    // })
 })
+function aaaaa(){
+	const domElements = $('.dropdown-toggle')[0]
+	domElements.onclick = function(e) {
+    	document.location.href = "file:///C:/Users/maksi/OneDrive/Документы/GitHub/freedom-hotels/room_list.html";
+    }
+}

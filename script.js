396ce2d8597b1ebaa2eefaaa6bacc83f0b65ffb4
MaksJohnson <kashinmaksim03@mail.ru>
2021-02-26 19:44:41 +0500

    aaaaa

--- script.js
+++ script.js
@@ -7,17 +7,33 @@ $(window).scroll(function() {
 });
 $(document).ready(function () {
     $('.dropdown-toggle').mouseover(function() {
-        $('.dropdown-menu').show();
+        $('.dropdown-menu').fadeIn();
     })
     $('.dropdown-toggle').mouseout(function() {
         t = setTimeout(function() {
-            $('.dropdown-menu').hide();
+            $('.dropdown-menu').fadeOut();
         }, 100);
         $('.dropdown-menu').on('mouseenter', function() {
-            $('.dropdown-menu').show();
+            $('.dropdown-menu').fadeIn();
             clearTimeout(t);
         }).on('mouseleave', function() {
-            $('.dropdown-menu').hide();
+            $('.dropdown-menu').fadeOut();
         })
     })
 })
+$(document).ready(function(){
+	$(window).scroll(function () {
+			if ($(this).scrollTop() > 50) {
+				$('#back-to-top').fadeIn();
+			} else {
+				$('#back-to-top').fadeOut();
+			}
+		});
+		// scroll body to 0px on click
+		$('#back-to-top').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 400);
+			return false;
+		});
+});
